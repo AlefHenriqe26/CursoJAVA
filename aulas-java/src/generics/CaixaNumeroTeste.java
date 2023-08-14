fcc0fa97b9b286b30d711dcc878315cba2e1d54b
alefHenriqe26 <alefhenriqe26@outlook.com>
2023-08-14 11:56:27 -0300

    Aula Generics

---
+++ aulas-java/src/generics/CaixaNumeroTeste.java
@@ -0,0 +1,14 @@
+package generics;
+
+public class CaixaNumeroTeste {
+
+	public static void main(String[] args) {
+		CaixaNumero<Double> caixaA = new CaixaNumero<>();
+		caixaA.guardar(2.3);
+		System.out.println(caixaA.abrir());
+		
+		CaixaNumero<Double> caixaB = new CaixaNumero<>();
+		caixaB.guardar(3.0);
+		System.out.println(caixaB.abrir());
+	}
+}
